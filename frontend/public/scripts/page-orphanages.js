daf5539ac0e0354958ff84dc8e7bb1c905e1fe63
devjapa <leandrotrovilho@gmail.com>
2020-10-17 01:02:00 -0300

    Add leallet map

--- frontend/public/scripts/page-orphanages.js
+++ frontend/public/scripts/page-orphanages.js
@@ -1,29 +1,27 @@
-//create map
+// create map
+const map = L.map("mapid").setView([-22.3711082, -46.0987629], 15);
 
-const map = L.map("mapid").setView([-16.6958288, -49.3743178], 14);
-
-//create and add tilelayer
-
+// create and add layer
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-//create icon
-
+// create icon
 const icon = L.icon({
   iconUrl: "./public/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [170, 2],
-});
+})
 
+//create popup overlay
 const popup = L.popup({
   closeButton: false,
-  className: "map-popup",
+  className: 'map-popup',
   minWidth: 240,
-  minHeight: 240,
-}).setContent(
-  'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>'
-);
+  minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
 
-//add marker
-
-L.marker([-16.6958288, -49.3743178], { icon }).addTo(map).bindPopup(popup);
+// create and add marker
+L.marker([-22.3711082, -46.0987629], { icon })
+  .addTo(map)
+  .bindPopup(popup);
+  
